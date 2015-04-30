f0262454bcd2c76a8246d9893f1e1998dd284bfe
Taewhi Lee <taewhi@gmail.com>
2015-04-30 21:52:10 +0900

    Fix octopus-core build failure

--- octopus-core/src/main/java/kr/co/bitnine/octopus/queryengine/QueryEngine.java
+++ octopus-core/src/main/java/kr/co/bitnine/octopus/queryengine/QueryEngine.java
@@ -4,6 +4,7 @@ import org.apache.calcite.schema.SchemaPlus;
 import org.apache.calcite.sql.SqlDialect;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.parser.SqlParseException;
+import org.apache.calcite.test.CalciteAssert;
 import org.apache.calcite.tools.FrameworkConfig;
 import org.apache.calcite.tools.Frameworks;
 import org.apache.calcite.tools.Planner;
